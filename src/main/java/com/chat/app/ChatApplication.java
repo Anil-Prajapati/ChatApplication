a802cb9f9a798d72aa59caf536e3ac674291cd43
=Anil-Prajapati <anilprajapati80814895@gmail.com>
2025-12-02 16:05:19 +0530

    Chat Application

--- src/main/java/com/chat/app/ChatApplication.java
+++ src/main/java/com/chat/app/ChatApplication.java
@@ -1,13 +1,29 @@
 package com.chat.app;
 
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ChatApplication {
+public class ChatApplication implements CommandLineRunner {
+
+    @Autowired
+    private Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        String[] activeProfiles = environment.getActiveProfiles();
+        System.out.println("=================================");
+        System.out.println("Active Profile: " + Arrays.toString(activeProfiles));
+        System.out.println("Database URL: " + environment.getProperty("spring.datasource.url"));
+        System.out.println("=================================");
+    }
 }
